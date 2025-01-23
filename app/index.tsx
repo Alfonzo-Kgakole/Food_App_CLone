fe96c07fd048ca3812c7b44070baf53bab8e4fe4
Alfonzo <thulanikgakole@gmail.com>
2025-01-23 13:05:47 +0200

    I have create the layout of the head and search section

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+            <View>
+                <Text>Index page</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+
+
+export default Page
